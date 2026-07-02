c8e4bc1849c4520403d9b52ca4f7d02122e53722
runoneall <runoobsteve@gmail.com>
2026-07-02 22:04:06 +0800

    Implement tag handling functions for XML processing

--- eval.go
+++ eval.go
@@ -1,5 +1,58 @@
 package main
 
-func onTagOpen() {}
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
-func onTagClose() {}
+func onTagOpen() {
+	n := i.CurrentNode()
+	if n == nil {
+		return
+	}
+
+	switch n.Name {
+
+	case "xml":
+		i.PushStack()
+
+	case "use":
+		i.PushStack()
+
+		if file := n.Attrs["file"]; file != "" {
+			f, err := os.Open(file)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			defer f.Close()
+			i.Run(f)
+
+		} else if url := n.Attrs["url"]; url != "" {
+			r, err := http.Get(url)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			defer r.Body.Close()
+			i.Run(r.Body)
+		}
+
+	}
+}
+
+func onTagClose() {
+	n := i.CurrentNode()
+	if n == nil {
+		return
+	}
+
+	switch n.Name {
+
+	case "log":
+		log.Println(n.Value)
+		i.PushStack()
+
+	}
+}
