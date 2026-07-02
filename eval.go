64368c8018f876b1daf0ee904f61341f56a81365
runoneall <runoobsteve@gmail.com>
2026-07-02 21:50:58 +0800

    Implement XML interpreter with error handling and add evaluation functions

---
+++ eval.go
@@ -0,0 +1,5 @@
+package main
+
+func onTagOpen() {}
+
+func onTagClose() {}
--- main.go
+++ main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/pybuild-org/xmlinterp"
+)
+
+var i *xmlinterp.Interpreter
+
+func main() {
+	var scriptPath string
+
+	args := os.Args
+	if len(args) != 2 {
+		if _, err := os.Stat("target.xml"); os.IsNotExist(err) {
+			log.Fatalln("usage: pybuild target.xml")
+		}
+
+		scriptPath = "target.xml"
+	} else {
+		scriptPath = args[1]
+	}
+
+	i = xmlinterp.New(onTagOpen, onTagClose)
+	f, err := os.Open(scriptPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer f.Close()
+	i.Run(f)
+}
