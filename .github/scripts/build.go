443caa514c52c6c207d33ee64c09c7750af4a927
runoneall <runoobsteve@gmail.com>
2026-07-05 10:34:25 +0800

    Implement build script to read platform configurations and update workflow to save dist list

--- .github/scripts/build.go
+++ .github/scripts/build.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Target struct {
+	GOOS   string `json:"GOOS"`
+	GOARCH string `json:"GOARCH"`
+}
+
+var Targets []Target
+
+func main() {
+	platforms, err := os.Open(".github/platforms.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := json.NewDecoder(platforms).Decode(&Targets); err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(Targets)
+}
